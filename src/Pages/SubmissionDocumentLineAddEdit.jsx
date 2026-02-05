34a7a3ed71f5e32219b9f03c9b7f3fab55469f83
montaser-hub <montaserismail20@gmail.com>
2026-02-05 15:16:13 +0200

    update

--- src/Pages/SubmissionDocumentLineAddEdit.jsx
+++ src/Pages/SubmissionDocumentLineAddEdit.jsx
@@ -1,178 +1,197 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import HeaderPageAddEdit from '../Components/HeaderPageAddEdit';
-import useHandleSubmit from '../Hooks/useHandleSubmit';
-import useGetById from '../Hooks/useGetById';
-import useLayout from '../Hooks/useLayout';
-import Loading from '../Components/loader';
-import { Form, Formik } from 'formik';
-import CustomInput from '../Components/Form/CustomInput';
-import CustomDatePicker from '../Components/Form/CustomDatePicker';
-import useGetGenerallist from '../Hooks/useGetGenerallist';
-import CustomTextarea from '../Components/Form/CustomTextarea';
-import useGetSelected from '../Hooks/useGetSelected';
-import AsyncSelectWrapper from '../Components/Form/AsyncSelectWrapper';
-import GenericGridPageLine from '../Components/GenericGridPageLine';
-import { DataPagesLine } from '../ConfigData/DataPagesLine';
 
+import React, { useEffect, useRef, useState } from "react";
+import { Form, Formik } from "formik";
+import PopupModalSlide from "../Components/PopupModalSlide";
+import useGetById from "../Hooks/useGetById";
+import CustomInput from "../Components/Form/CustomInput";
+import AsyncSelectWrapper from "../Components/Form/AsyncSelectWrapper";
+import useHandleSubmit from "../Hooks/useHandleSubmit";
+import Config from "../utils/Config";
 
+/**
+ * SubmissionDocumentLineAddEdit
+ *
+ * This component renders a form to add or edit data. It accepts the following props:
+ *
+ * - recId: The id of the record to be edited. If not provided, the component will render a form to add a new record.
+ * - isVisible: A boolean indicating whether the component is visible or not.
+ * - ApiPage: The API endpoint to be used for add or edit operations.
+ * - toggleClick: A function to be called when the component is closed.
+ * - titleSubmitBtn: The text to be displayed on the submit button.
+ * - title: The title of the component.
+ * - iconModal: The icon to be used in the modal header.
+ * - fetchGridData: A function to be called when the component is submitted.
+ * - ResourcePage: The resource page to be used for validation and error messages.
+ *
+ * The component will fetch data from the API if recId is provided and isVisible is true. It will then render a form with the following fields:
+ *
+ * - name: A text input field for the name of the record.
+ * - createdOn: A date picker field for the creation date of the record.
+ *
+ * The component will validate the fields using the NameSchema and render error messages if any of the fields are invalid.
+ *
+ * When the form is submitted, the component will call the handleSubmitFormik function from the useHandleSubmit hook, passing the form values and the other props as arguments.
+ *
+ * The component will also call the fetchGridData function when the form is submitted, passing the recId and ResourcePage as arguments.
+ **/
+export default function SubmissionDocumentLineAddEdit({
+  recId,
+  isVisible,
+  ApiPage,
+  toggleClick,
+  titleSubmitBtn,
+  title,
+  iconModal,
+  fetchGridData,
+  ResourcePage,
+  ConfiMainPage,
+}) {
+  const formikRef = useRef();
+  const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingSubmit, setIsLoadingSubmit] = useState(false);
+  const [data, setData] = useState({});
+  const [unit, setUnit] = useState("");
+  const isAllowedModify = Config.isAllow("Modify", ConfiMainPage);
 
+  const fetchData = useGetById(
+    `${ApiPage}`,
+    recId,
+    setIsLoading,
+    setData,
+    null,
+    ResourcePage
+  );
+  const { handleSubmitFormik } = useHandleSubmit();
 
-export const SubmissionDocumentLineAddEdit = ({ DataPage, ResourcePage, ...props }) => {
-    useLayout(ResourcePage);
-    const { id } = useParams();
-    const navigate = useNavigate();
-    const { getGenerallist } = useGetGenerallist();
-    const [isLoading, setIsLoading] = useState(true);
-    const [isSubmitting, setIsSubmitting] = useState(false);
-    const [data, setData] = useState({});
-    const [unit, setUnit] = useState("");
-    const formRef = React.useRef();
-    const { handleSubmitFormik } = useHandleSubmit();
-    const fetchData = useGetById(
-        DataPage.Api,
-        id,
-        setIsLoading,
-        setData,
-        null,
-        ResourcePage
-    );
-    useEffect(() => {
-        if (id && id !== "0") {
-            fetchData();
-        } else {
-            setIsLoading(false);
-        }
-    }, [id, DataPage.Api]);
-    const handleSave = async (values) => {
-        const currentPath = window.location.pathname;
-        const basePath = currentPath.split('/').slice(0, -2).join('/'); // Naive back path
-        const sendData = {
-            parentRecId: values.parentRecId,
-            purchaceQuantity: values.purchaceQuantity,
-            itemRecId: values.itemRecId,
-            departmentRecId: values.departmentRecId,
-        }
-        await handleSubmitFormik({
-            apiPage: DataPage.Api,
-            values: sendData,
-            recId: id,
-            resourcePage: ResourcePage,
-            setIsLoadingSubmit: setIsSubmitting,
-            setData: setData,
-            onSuccess: () => navigate(basePath),
-            fetchData: fetchData
-        });
+  useEffect(() => {
+    if (isVisible && recId > 0) {
+      fetchData();
+    } else {
+      setIsLoading(false);
+    }
+  }, [recId, isVisible]);
+
+  const handleSubmit = (values) => {
+    setIsLoadingSubmit(true);
+    const sendData = {
+        parentRecId: values.parentRecId,
+        purchaceQuantity: values.purchaceQuantity,
+        itemRecId: values.itemRecId,
+        departmentRecId: values.departmentRecId,
     };
-    const handleBack = () => {
-        navigate(-1);
-    };
-    if (isLoading) return <Loading />;
-    const isEdit = id && id !== "0";
-    return (
-        <>
-            <div className='flex flex-col bg-bgColor dark:bg-bgColorDark'>
-                <HeaderPageAddEdit
-                    option={isEdit ? "edit" : "add"}
-                    id={id}
-                    apiKey={DataPage.Api}
-                    ResourcePage={ResourcePage}
-                    titleAdd={`${ResourcePage} Info`}
-                    titleEdit={`${ResourcePage} Info`}
-                    goBackPrev={handleBack}
-                    onSubmit={() => {
-                        if (formRef.current) {
-                            formRef.current.submitForm();
-                        }
-                    }}
-                    isLoadingSubmit={isSubmitting}
-                    showBookmark={false}
-                    viewOnly={false}
-                />
-                <div className="px-4 py-6">
-                    <Formik
-                        innerRef={formRef}
-                        enableReinitialize={true}
-                        initialValues={{
-                            parentRecId: data.parentRecId || "",
-                            purchaceQuantity: data.purchaceQuantity || "",
-                            itemRecId: data.itemRecId || "",
-                            departmentRecId: data.departmentRecId || "",
-                        }}
-                        onSubmit={(values) =>
-                            handleSave(values)
-                        }
-                    >
-                        {({
-                            handleSubmit,
-                            handleChange,
-                            handleBlur,
-                            setFieldValue,
-                            values,
-                            errors,
-                            touched,
-                            setValues,
-                        }) => (
-                            <Form
-                                autoComplete="off"
-                                noValidate="noValidate"
-                                onSubmit={handleSubmit}
-                                className='grid grid-cols-2 gap-x-6 gap-y-6'
-                            >
-                                <AsyncSelectWrapper
-                                    name="itemRecId"
-                                    label="Item"
-                                    required
-                                    value={values.itemRecId}
-                                    onChange={(val) => setFieldValue('itemRecId', val)}
-                                    lookup={{
-                                        api: 'Item/GetLookup',
-                                        valueKey: 'recId',
-                                        labelKey: 'itemNumber'
-                                    }}
-                                    onSelectionChange={(selected) => {
-                                        setUnit(selected?.original?.inventoryUnitSymbol || "UNKNOWN");
-                                    }}
-                                    error={errors.itemRecId}
-                                    touched={touched.itemRecId}
-                                />
-                                <CustomInput
-                                    name="unit"
-                                    label="Unit"
-                                    value={unit}
-                                    readOnly={true}
-                                    disabled={true}
-                                />
-                                <CustomInput
-                                    name="purchaceQuantity"
-                                    label="Purchace Quantity"
-                                    type="number"
-                                    value={values.purchaceQuantity}
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    error={errors.purchaceQuantity}
-                                    touched={touched.purchaceQuantity}
-                                    required
-                                />
-                                <AsyncSelectWrapper
-                                    name="departmentRecId"
-                                    label="Department"
-                                    required
-                                    value={values.departmentRecId}
-                                    onChange={(val) => setFieldValue('departmentRecId', val)}
-                                    lookup={{
-                                        api: 'Department/GetLookup',
-                                        valueKey: 'operatingUnitNumber',
-                                        labelKey: 'name'
-                                    }}
-                                    error={errors.departmentRecId}
-                                    touched={touched.departmentRecId}
-                                />
-                            </Form>
-                        )}
-                    </Formik>
-                </div>
-            </div>
-        </>
-    );
-};
+
+    handleSubmitFormik({
+      apiPage: ApiPage,
+      values: sendData,
+      recId: recId,
+      resourcePage: ResourcePage,
+      setIsLoadingSubmit: setIsLoadingSubmit,
+      onSuccess: () => {
+          toggleClick();
+          if (fetchGridData) fetchGridData();
+      },
+    });
+  };
+
+  return (
+    <PopupModalSlide
+      modalSize="w-96"
+      isVisible={isVisible}
+      toggleClick={() => {
+        setData({});
+        setUnit("");
+        toggleClick();
+      }}
+      submitClick={() => formikRef.current?.handleSubmit()}
+      isLoadingSubmit={isLoadingSubmit}
+      isLoading={isLoading}
+      icon={iconModal}
+      titleSubmitBtn={titleSubmitBtn}
+      title={title}
+      viewOnly={!isAllowedModify}
+      ResourcePage={ResourcePage}
+      titleCancel="cancel"
+    >
+      <Formik
+        innerRef={formikRef}
+        initialValues={{
+          parentRecId: data?.parentRecId || "",
+          purchaceQuantity: data?.purchaceQuantity || "",
+          itemRecId: data?.itemRecId || "",
+          departmentRecId: data?.departmentRecId || "",
+        }}
+        enableReinitialize={true}
+        onSubmit={(values) => handleSubmit(values)}
+      >
+        {({
+          handleSubmit,
+          handleChange,
+          handleBlur,
+          setFieldValue,
+          values,
+          errors,
+          touched,
+        }) => (
+          <Form
+            autoComplete="off"
+            noValidate="noValidate"
+            onSubmit={handleSubmit}
+            className="grid grid-cols-1 gap-4"
+          >
+            <fieldset disabled={!isAllowedModify} className="grid grid-cols-1 gap-4">
+              <AsyncSelectWrapper
+                name="itemRecId"
+                label="Item"
+                required
+                value={values.itemRecId}
+                onChange={(val) => setFieldValue('itemRecId', val)}
+                lookup={{
+                    api: 'Item/GetLookup',
+                    valueKey: 'recId',
+                    labelKey: 'itemNumber'
+                }}
+                onSelectionChange={(selected) => {
+                    setUnit(selected?.original?.inventoryUnitSymbol || "UNKNOWN");
+                }}
+                error={errors.itemRecId}
+                touched={touched.itemRecId}
+              />
+              <CustomInput
+                name="unit"
+                label="Unit"
+                value={unit}
+                readOnly={true}
+                disabled={true}
+              />
+              <CustomInput
+                name="purchaceQuantity"
+                label="Purchace Quantity"
+                type="number"
+                value={values.purchaceQuantity}
+                onChange={handleChange}
+                onBlur={handleBlur}
+                error={errors.purchaceQuantity}
+                touched={touched.purchaceQuantity}
+                required
+              />
+              <AsyncSelectWrapper
+                name="departmentRecId"
+                label="Department"
+                required
+                value={values.departmentRecId}
+                onChange={(val) => setFieldValue('departmentRecId', val)}
+                lookup={{
+                    api: 'Department/GetLookup',
+                    valueKey: 'operatingUnitNumber',
+                    labelKey: 'name'
+                }}
+                error={errors.departmentRecId}
+                touched={touched.departmentRecId}
+              />
+            </fieldset>
+          </Form>
+        )}
+      </Formik>
+    </PopupModalSlide>
+  );
+}
